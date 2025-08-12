7175c32a1fef83b04335f80dd0e0f09dae1c1d1b
tonotom1 <tonotom@proton.me>
2025-08-13 02:48:38 +1000

    Botany Genetic Manipulation Buff (#1878)
    * botany dining
    
    * comments
    
    * chore: automatically update REUSE headers
    
    ---------
    
    Co-authored-by: monolith8319 <195513600+monolith8319@users.noreply.github.com>

--- Content.Server/Botany/Components/BotanySwabComponent.cs
+++ Content.Server/Botany/Components/BotanySwabComponent.cs
@@ -1,3 +1,10 @@
+// SPDX-FileCopyrightText: 2022 Kevin Zheng
+// SPDX-FileCopyrightText: 2023 DrSmugleaf
+// SPDX-FileCopyrightText: 2023 keronshb
+// SPDX-FileCopyrightText: 2025 tonotom
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using System.Threading;
 
 namespace Content.Server.Botany
@@ -9,7 +16,7 @@ namespace Content.Server.Botany
     public sealed partial class BotanySwabComponent : Component
     {
         [DataField("swabDelay")]
-        public float SwabDelay = 2f;
+        public float SwabDelay = 1f; //Mono: 2f >> 1f
 
         /// <summary>
         /// SeedData from the first plant that got swabbed.
