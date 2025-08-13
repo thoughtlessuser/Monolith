c5820479f296ee27cc365142edd149efd47c6a1c
ScyronX <166930367+ScyronX@users.noreply.github.com>
2025-08-13 18:45:18 +0300

    Language Fix (#1890)
    * language fix
    
    * chore: automatically update REUSE headers
    
    * fix
    
    * chore: automatically update REUSE headers
    
    * fix
    
    * chore: automatically update REUSE headers
    
    ---------
    
    Co-authored-by: monolith8319 <195513600+monolith8319@users.noreply.github.com>

---
+++ Content.Server/_Mono/Jobs/AddLanguageSpecial.cs
@@ -0,0 +1,57 @@
+// SPDX-FileCopyrightText: 2025 ScyronX
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using Content.Shared.Roles;
+using Content.Server._EinsteinEngines.Language;
+
+namespace Content.Server.Jobs;
+
+public sealed partial class AddLanguageSpecial : JobSpecial
+{
+    /// <summary>
+    ///     The list of all Spoken Languages that this trait adds.
+    /// </summary>
+    [DataField]
+    public List<string>? LanguagesSpoken { get; private set; } = default!;
+
+    /// <summary>
+    ///     The list of all Understood Languages that this trait adds.
+    /// </summary>
+    [DataField]
+    public List<string>? LanguagesUnderstood { get; private set; } = default!;
+
+    /// <summary>
+    ///     The list of all Spoken Languages that this trait removes.
+    /// </summary>
+    [DataField]
+    public List<string>? RemoveLanguagesSpoken { get; private set; } = default!;
+
+    /// <summary>
+    ///     The list of all Understood Languages that this trait removes.
+    /// </summary>
+    [DataField]
+    public List<string>? RemoveLanguagesUnderstood { get; private set; } = default!;
+
+    public override void AfterEquip(EntityUid mob)
+    {
+        var language = IoCManager.Resolve<IEntitySystemManager>().GetEntitySystem<LanguageSystem>();
+
+
+        if (RemoveLanguagesSpoken is not null)
+            foreach (var lang in RemoveLanguagesSpoken)
+                language.RemoveLanguage(mob, lang, true, false);
+
+        if (RemoveLanguagesUnderstood is not null)
+            foreach (var lang in RemoveLanguagesUnderstood)
+                language.RemoveLanguage(mob, lang, false, true);
+
+        if (LanguagesSpoken is not null)
+            foreach (var lang in LanguagesSpoken)
+                language.AddLanguage(mob, lang, true, false);
+
+        if (LanguagesUnderstood is not null)
+            foreach (var lang in LanguagesUnderstood)
+                language.AddLanguage(mob, lang, false, true);
+    }
+}
