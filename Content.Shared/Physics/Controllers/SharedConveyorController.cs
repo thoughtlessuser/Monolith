4d475af1e762ad4485c46ef64c8e376ba0e52cab
Ilya246 <57039557+Ilya246@users.noreply.github.com>
2025-08-25 00:03:28 +0400

    Wizden #37466 Port: Ensure Large Entities Don't Get Stuck on Conveyor Belt Corners (#2025)
    * Ensure large entities don't get stuck on conveyor belt corners (#37466)
    
    * Initial commit
    
    * Also avoid the friction change doesn't make turned-off conveyor belts continue going
    
    * Ensurecomp on conveyor starting
    
    * i give up
    
    * Minor brackets edit
    
    * Documentation
    
    * chore: automatically update REUSE headers
    
    ---------
    
    Co-authored-by: SlamBamActionman <83650252+SlamBamActionman@users.noreply.github.com>
    Co-authored-by: monolith8319 <195513600+monolith8319@users.noreply.github.com>

--- Content.Shared/Physics/Controllers/SharedConveyorController.cs
+++ Content.Shared/Physics/Controllers/SharedConveyorController.cs
@@ -4,6 +4,8 @@
 // SPDX-FileCopyrightText: 2023 keronshb
 // SPDX-FileCopyrightText: 2024 Tayrtahn
 // SPDX-FileCopyrightText: 2024 TemporalOroboros
+// SPDX-FileCopyrightText: 2025 Redrover1760
+// SPDX-FileCopyrightText: 2025 SlamBamActionman
 // SPDX-FileCopyrightText: 2025 metalgearsloth
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
@@ -106,6 +108,11 @@ public abstract class SharedConveyorController : VirtualController
         {
             var other = contact.OtherEnt(conveyorUid);
 
+            if (contact.OtherFixture(conveyorUid).Item2.Hard && contact.OtherBody(conveyorUid).BodyType != BodyType.Static)
+            {
+                EnsureComp<ConveyedComponent>(other);
+            }
+
             if (_conveyedQuery.HasComp(other))
             {
                 PhysicsSystem.WakeBody(other);
@@ -165,7 +172,9 @@ public abstract class SharedConveyorController : VirtualController
                 // they'll go too slow.
                 if (!_mover.UsedMobMovement.TryGetValue(ent.Entity.Owner, out var usedMob) || !usedMob)
                 {
-                    _mover.Friction(0f, frameTime: frameTime, friction: 5f, ref velocity);
+                    // We provide a small minimum friction speed as well for those times where the friction would stop large objects
+                    // snagged on corners from sliding into the centerline.
+                    _mover.Friction(0.2f, frameTime: frameTime, friction: 5f, ref velocity);
                 }
 
                 SharedMoverController.Accelerate(ref velocity, targetDir, 20f, frameTime);
@@ -328,7 +337,9 @@ public abstract class SharedConveyorController : VirtualController
         var p = direction * (Vector2.Dot(itemRelative, direction) / Vector2.Dot(direction, direction));
         var r = itemRelative - p;
 
-        if (r.Length() < 0.1)
+        // 0.01 is considered close enough to the centerline that (most) large objects shouldn't
+        // snag on walls next to the conveyor, without smaller entities repeatedly overshooting.
+        if (r.Length() < 0.01)
         {
             var velocity = direction * speed;
             return velocity;
@@ -338,7 +349,10 @@ public abstract class SharedConveyorController : VirtualController
             // Give a slight nudge in the direction of the conveyor to prevent
             // to collidable objects (e.g. crates) on the locker from getting stuck
             // pushing each other when rounding a corner.
-            var velocity = (r + direction).Normalized() * speed;
+            // The direction of the conveyorbelt is de-emphasized to ensure offset objects primarily push centerwards,
+            // to prevent large items getting snagged on corner turns.
+            // 0.2f seems like a good compromise between forwards and sideways movement.
+            var velocity = (r + direction * 0.2f).Normalized() * speed;
             return velocity;
         }
     }
@@ -392,7 +406,7 @@ public abstract class SharedConveyorController : VirtualController
 
             var other = contact.OtherEnt(ent.Owner);
 
-            if (_conveyorQuery.HasComp(other))
+            if (_conveyorQuery.TryComp(other, out var comp) && CanRun(comp))
                 return true;
         }
 
