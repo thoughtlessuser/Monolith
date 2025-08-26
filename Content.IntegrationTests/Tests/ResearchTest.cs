107a3a6e41aede3437660a1bd6c64ce33ee28121
Redrover1760 <39284090+Redrover1760@users.noreply.github.com>
2025-08-25 20:37:07 -0400

    Research Test Now Accounts for Tech Disks (#2052)
    * tech disk research fix
    
    * chore: automatically update REUSE headers
    
    ---------
    
    Co-authored-by: Redrover1760 <gnarlysheep.personal@gmail.com>
    Co-authored-by: monolith8319 <195513600+monolith8319@users.noreply.github.com>

--- Content.IntegrationTests/Tests/ResearchTest.cs
+++ Content.IntegrationTests/Tests/ResearchTest.cs
@@ -1,9 +1,21 @@
+// SPDX-FileCopyrightText: 2023 Leon Friedrich
+// SPDX-FileCopyrightText: 2023 Nemanja
+// SPDX-FileCopyrightText: 2023 TemporalOroboros
+// SPDX-FileCopyrightText: 2023 Visne
+// SPDX-FileCopyrightText: 2023 metalgearsloth
+// SPDX-FileCopyrightText: 2024 Tayrtahn
+// SPDX-FileCopyrightText: 2025 Redrover1760
+// SPDX-FileCopyrightText: 2025 deltanedas
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using System.Collections.Generic;
 using System.Linq;
 using Content.Shared.Lathe;
 using Content.Shared.Research.Prototypes;
 using Robust.Shared.GameObjects;
 using Robust.Shared.Prototypes;
+using Content.Shared.Research.TechnologyDisk.Components;
 
 namespace Content.IntegrationTests.Tests;
 
@@ -91,11 +103,29 @@ public sealed class ResearchTest
                         Assert.That(latheTechs, Does.Contain(recipe), $"Recipe '{recipe}' from tech '{tech.ID}' cannot be unlocked on any lathes.");
                     }
                 }
+                // Mono: Tech Disk Check.
+                var techDiskRecipes = new HashSet<ProtoId<LatheRecipePrototype>>();
+                foreach (var proto in allEnts)
+                {
+                    if (proto.Abstract)
+                        continue;
 
+                    if (pair.IsTestPrototype(proto))
+                        continue;
+
+                    if (!proto.TryGetComponent<TechnologyDiskComponent>(out var techDisk, compFact))
+                        continue;
+
+                    if (techDisk.Recipes != null)
+                        techDiskRecipes.UnionWith(techDisk.Recipes);
+                }
+
+                unlockedTechs.UnionWith(techDiskRecipes);
                 // now check that every dynamic recipe a lathe lists can be unlocked
+                // mono: or has a tech disk recipe
                 foreach (var recipe in latheTechs)
                 {
-                    Assert.That(unlockedTechs, Does.Contain(recipe), $"Recipe '{recipe}' is dynamic on a lathe but cannot be unlocked by research.");
+                    Assert.That(unlockedTechs, Does.Contain(recipe), $"Recipe '{recipe}' is dynamic on a lathe but cannot be unlocked by research or tech disk.");
                 }
             });
         });
