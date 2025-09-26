fd970c9333ca25d03538e3c00675522d190e9e55
starch <starchpersonal@gmail.com>
2025-09-26 17:04:26 -0400

    [Bounty] Exo Tiles/Walls (#2343)
    * Wow, i hate these walking noises
    
    * noob
    
    * Yeah
    
    * chore: automatically update REUSE headers
    
    * Forgot
    
    * kys
    
    * chore: automatically update REUSE headers
    
    * chore: automatically update REUSE headers
    
    ---------
    
    Co-authored-by: SlamBamActionman <83650252+SlamBamActionman@users.noreply.github.com>
    Co-authored-by: monolith8319 <195513600+monolith8319@users.noreply.github.com>

--- Content.Shared/Maps/ContentTileDefinition.cs
+++ Content.Shared/Maps/ContentTileDefinition.cs
@@ -33,8 +33,10 @@
 // SPDX-FileCopyrightText: 2024 PECK
 // SPDX-FileCopyrightText: 2025 Ark
 // SPDX-FileCopyrightText: 2025 Princess Cheeseballs
+// SPDX-FileCopyrightText: 2025 Redrover1760
 // SPDX-FileCopyrightText: 2025 Tayrtahn
 // SPDX-FileCopyrightText: 2025 mikusssssss
+// SPDX-FileCopyrightText: 2025 starch
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
@@ -119,6 +121,11 @@ namespace Content.Shared.Maps
         [DataField("friction")] public float Friction { get; set; } = 1f;
 
         [DataField("variants")] public byte Variants { get; set; } = 1;
+
+        /// <summary>
+        ///     Allows the tile to be rotated/mirrored when placed on a grid.
+        /// </summary>
+        [DataField] public bool AllowRotationMirror { get; set; } = false;
 
         /// <summary>
         /// This controls what variants the `variantize` command is allowed to use.
---
+++ Resources/Locale/en-US/stack/stacks.ftl
@@ -0,0 +1,6 @@
+stack-techmaint-floor-dark = dark techmaint floor
+stack-xeno-floor = xeno floor
+stack-xeno-steel = xeno steel tile
+stack-xeno-steel-corner = xeno steel corner tile
+stack-xeno-maint = xeno techmaint
+stack-dark-squiggly = dark steel squiggly tile
