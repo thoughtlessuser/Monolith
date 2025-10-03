62601ca3d74e532e16acf1d9a78b4ae3db12d030
KOTOB <59124164+kotobdev@users.noreply.github.com>
2025-10-02 20:46:10 -0700

    Make Asakim Armor Practically Unusable by Other Species (#2372)
    * gaming
    
    * chore: automatically update REUSE headers
    
    ---------
    
    Co-authored-by: monolith8319 <195513600+monolith8319@users.noreply.github.com>

---
+++ Content.Shared/_DV/Clothing/Components/ClothingSlowResistanceComponent.cs
@@ -0,0 +1,17 @@
+// SPDX-FileCopyrightText: 2025 kotobdev
+//
+// SPDX-License-Identifier: MPL-2.0
+
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._DV.Clothing.Components;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class ClothingSlowResistanceComponent : Component
+{
+    /// <summary>
+    /// Modifier for both walk and sprint slowdown.
+    /// </summary>
+    [DataField(required: true)]
+    public float Modifier = 0.25f;
+}
---
+++ Content.Shared/_DV/Clothing/Events/ModifyClothingSlowdownEvent.cs
@@ -0,0 +1,13 @@
+// SPDX-FileCopyrightText: 2025 kotobdev
+//
+// SPDX-License-Identifier: MPL-2.0
+
+namespace Content.Shared._DV.Clothing.Events;
+
+/// <summary>
+/// Raised on an entity when clothing would slow them down or when they examine clothing.
+/// </summary>
+/// <param name="WalkModifier">The walking speed modifier of the clothing.</param>
+/// <param name="RunModifier">The running speed modifier of the clothing.</param>
+[ByRefEvent]
+public record struct ModifyClothingSlowdownEvent(float WalkModifier, float RunModifier);
